ede9f56db5b3c5e1445f0a67dca1789b495450b7
KeeganCorrigan <keegan.rw.corrigan@gmail.com>
2018-09-12 09:42:22 -0600

    adds api call with correct results, builds out controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,13 +1,25 @@
 class WelcomeController < ApplicationController
   def index
-    conn = Faraday.new(url: "https://od-api.oxforddictionaries.com") do |faraday|
-              faraday.headers["app_key"] ="#{ENV["APP_KEY"]}"
-              faraday.headers["app_id"] = "#{ENV["APP_ID"]}"
-              faraday.adapter Faraday.default_adapter
-            end
+    if params[:q]
+      conn = Faraday.new(url: "https://od-api.oxforddictionaries.com") do |faraday|
+                faraday.headers["app_key"] ="#{ENV["APP_KEY"]}"
+                faraday.headers["app_id"] = "#{ENV["APP_ID"]}"
+                faraday.adapter Faraday.default_adapter
+              end
 
-    response = conn.get("https://od-api.oxforddictionaries.com:443/api/v1/inflections/en/#{}")
+      response = conn.get("/api/v1/inflections/en/#{params[:q]}/grammaticalFeatures=singular")
 
+      payload = JSON.parse(response.body, symbolize_names: true)
 
+      binding.pry
+
+      payload[:results].first[:lexicalEntries][1][:inflectionOf].first[:id]
+    end
+
+      @valid_word = if payload[:results]
+                      return "'#{params[:q]}' is a valid word and its root form is #{payload[:results].first[:lexicalEntries][0][:inflectionOf].first[:id]}."
+                    elsif !payload[:results]
+                      return "'#{params[:q]}' is not a valid word."
+                    end
   end
 end
